06b5959c8a6e4155a181b40ca00f6646f4acac35
Mubyyy404 <mubeenmass577@gmail.com>
2025-10-14 23:21:03 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -20,7 +20,9 @@ const storage = getStorage(app);
 
 // Global auth state listener for redirects
 onAuthStateChanged(auth, (user) => {
+  console.log('Auth state changed:', user ? `User: ${user.uid}, Email: ${user.email}, DisplayName: ${user.displayName}` : 'No user');
   if (!user && !window.location.pathname.includes('login.html') && !window.location.pathname.includes('register.html')) {
+    console.log('Redirecting to login.html from:', window.location.pathname);
     window.location.href = "login.html";
   }
 });
